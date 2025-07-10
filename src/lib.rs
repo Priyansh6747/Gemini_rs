0b5275f955cfaf01b34a3f0871c58cba62a8f7e5
Priyansh Singh Tanwar <154755072+Priyansh6747@users.noreply.github.com>
2025-07-10 16:30:46 +0530

    Add Tokio dependency and async test for Client
    Added the 'tokio' crate to dependencies and implemented an async test using #[tokio::test] to verify the Client's chat functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,18 @@ mod models;
 mod api;
 mod client;
 
+use std::error::Error;
 pub use client::Client;
 pub use models::*;
+
+#[tokio::test]
+async fn trying() {
+    let mut c = Client::new("key", "gemini-2.5-flash");
+    let res = c.chat("Hello my name is Max").await;
+    if res.is_err() {
+    panic!("{}", res.err().unwrap());}
+    let res2 = c.chat("Whats my name answer in single word only (answer containing more than 1 word will be treated as incorrect)").await;
+    let name = res2.unwrap_or_else(|_| "Error".to_string());
+    dbg!(&name);
+    assert_eq!(&name, "Max");
+}
